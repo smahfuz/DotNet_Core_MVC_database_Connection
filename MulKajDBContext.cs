1103f41359def3b56fe850a3fb772784763fbf45
Mahfuz Ahammed <79596013+smahfuz@users.noreply.github.com>
2023-12-17 23:17:43 +0600

    Add files via upload
    added some files....

---
+++ MulKajDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SecondPractice.DBContext
+{
+    public class MulKajDBContext : DbContext
+    {
+        public MulKajDBContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<MyDataTypesAre> elomelo { get; set; }
+    }
+}
---
+++ MyDataTypesAre.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SecondPractice.DBContext
+{
+    public class MyDataTypesAre
+    {
+        [Key]
+        public int ID { get; set; }
+        [Column(TypeName = "varchar(200)")]
+        public string District { get; set; }
+
+        [Column(TypeName = "varchar(200)")]
+        public string Village { get; set; }
+    }
+}
